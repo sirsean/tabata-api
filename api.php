6187bce0451d83097fc2f52e5e8201059492abcc
Sean Schulte <sirsean@gmail.com>
2011-02-25 20:51:31 -0600

    Changed the JSON return format.

--- api.php
+++ api.php
@@ -11,13 +11,13 @@ $password = $_POST['password'];
 try {
     if ($method == "checkAuthentication") {
         $user = checkAuthentication($email, $password);
-        $rval = true;
+        echo json_encode(array("success" => true));
     } else if ($method == "createUser") {
         try {
             $existing_user = getUserByEmail($email);
             throw new Exception("User exists");
         } catch (UserNotFoundException $e) {
-            $rval = createUser($email, $password);
+            echo json_encode(array("success" => true));
         }
     } else if ($method == "addHistory") {
         $user = checkAuthentication($email, $password);
@@ -26,15 +26,14 @@ try {
             throw new Exception("Invalid parameters");
         }
 
-        $rval = addHistory($user->id, $_POST["sprintSeconds"], $_POST["restSeconds"], $_POST["numSprints"]);
+        addHistory($user->id, $_POST["sprintSeconds"], $_POST["restSeconds"], $_POST["numSprints"]);
+        echo json_encode(array("success" => true));
     } else if ($method == "getHistory") {
         $user = checkAuthentication($email, $password);
-        $rval = getHistory($user->id);
+        echo json_encode(getHistory($user->id));
     } else {
         throw new Exception("Unknown method");
     }
-
-    echo json_encode(array("response" => $rval));
 } catch (Exception $e) {
     echo json_encode(array("error" => $e->getMessage()));
 }
